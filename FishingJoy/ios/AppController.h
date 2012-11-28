b2dcff5bda6446e44e99dda772e647f5425eb2bd
dingjiecn <dingjiecn@gmail.com>
2012-11-28 10:25:38 +0800

    add project

---
+++ FishingJoy/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  FishingJoyAppController.h
+//  FishingJoy
+//
+//  Created by jie ding on 11/28/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ FishingJoy/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FishingJoyAppController.h
+//  FishingJoy
+//
+//  Created by jie ding on 11/28/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
